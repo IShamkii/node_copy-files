c1a0a2d4f009fbb98c3ad1adfafac886da7ff3d7
IShamkii <fanmetalheavy@gmail.com>
2025-02-05 18:57:07 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const fs = require('fs');
+
+function copyFile() {
+  const args = process.argv.slice(2);
+  const pathToCopyFile = args[0];
+  const pathToInsertFile = args[1];
+
+  if (pathToCopyFile === pathToInsertFile) {
+    return;
+  }
+
+  try {
+    fs.cpSync(pathToCopyFile, pathToInsertFile);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('Error: ', err.code);
+  }
+}
+
+copyFile();
